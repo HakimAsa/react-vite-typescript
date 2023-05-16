ee2d7b5b0bacc0c002efd08b61b10d7b9ccadf8e
HakimAsa <igbo7777@gmail.com>
2023-05-16 12:03:40 +0100

    passing children

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import ListGroup from './components/ListGroup';
+// import ListGroup from './components/ListGroup';
+
+import Alert from './components/Alert';
 
 function App() {
-  const cities = ['Cotonou', 'Parakou', 'Djougou', 'Bohicon', 'Nikki'];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  // const cities = ['Cotonou', 'Parakou', 'Djougou', 'Bohicon', 'Nikki'];
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
   return (
     <div>
-      <ListGroup items={cities} heading='List of certain Benin Republic Cities' onSelectItem={handleSelectItem} />
+      {/* <ListGroup items={cities} heading='List of certain Benin Republic Cities' onSelectItem={handleSelectItem} /> */}
+      <Alert>Hello Children</Alert>
     </div>
   );
 }
